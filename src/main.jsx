ca6046cd293c1f4a5b4c84693f4a19f86520954e
semyhist <Semih.aydin142@gmail.com>
2026-03-06 19:24:13 +0300

    main.jsx'e eski service worker'ları silen kod eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
